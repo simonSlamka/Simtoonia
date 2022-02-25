c3c60a6c52e5837178004f2f8760b537bab762f5
Simon Slamka <51794014+simonSlamka@users.noreply.github.com>
2022-02-25 16:20:10 +0100

    init xcode proj;

---
+++ Simtoonia/Simtoonia/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Simtoonia
+//
+//  Created by Simon Slamka on 2/25/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Simtoonia/Simtoonia/SimtooniaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimtooniaApp.swift
+//  Simtoonia
+//
+//  Created by Simon Slamka on 2/25/22.
+//
+
+import SwiftUI
+
+@main
+struct SimtooniaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
